19e7fdb0b55caefb37275d43c3de8313df1aeec9
Sebastian Mattias Rodriguez <seba.rodri.29@gmail.com>
2023-05-18 14:22:44 +0200

    Done exercise 3-4-5. Use more string of names to greet persons

--- Greeting.Test/UnitTest1.cs
+++ Greeting.Test/UnitTest1.cs
@@ -26,4 +26,24 @@ public class GreetingTest
     {
         Assert.Equal("Hello, my friend", _sut.Greet(null));
     }
+
+    [Fact]
+    public void ShouldReturnGreetShouted()
+    {
+        Assert.Equal("HELLO BOB!", _sut.ShoutedGreet("BOB"));
+    }
+
+    [Fact]
+    [InlineData(2, new[] { "Jill", "Jane" })]
+    public void ShouldGreetTwoPerson()
+    {
+        Assert.Equal("Hello, Jill and Jane", _sut.GreetTwoPerson("Jill", "Jane"));
+    }
+
+    [Fact]
+    [InlineData(3,new[] { "Amy", "Brian", "Charlotte" })]
+    public void ShouldGreetMorePerson()
+    {
+        Assert.Equal("Hello, Amy, Brian, and Charlotte.", _sut.GreetMorePerson("Amy", "Brian", "Charlotte"));
+    }
 }
--- Greeting/Greeting.cs
+++ Greeting/Greeting.cs
@@ -18,6 +18,45 @@ namespace Greeting
 				return "Hello, my friend";
 			}
 		}
-	}
+
+		public string ShoutedGreet(string name)
+		{
+			for (int i = 0; i < name.Length; i++)
+			{
+				if (!char.IsUpper(name[i]))
+				{
+					return "Name not shouted";
+				}
+			}
+
+			return $"HELLO {name}!";
+		}
+
+        public string GreetTwoPerson(params string[] names)
+		{
+			var greet = $"Hello, {names[0]} and {names[1]}";
+
+			return greet;
+		}
+
+        public string GreetMorePerson(params string[] names)
+        {
+			var greet = "Hello, ";
+
+			for (int i = 0; i < names.Length; i++)
+			{
+				if (i < (names.Length-1))
+				{
+					greet = greet + $"{names[i]}, ";
+				}
+				else
+				{
+					greet = greet + $"and {names[i]}.";
+				}
+			}
+
+			return greet;
+        }
+    }
 }
 
