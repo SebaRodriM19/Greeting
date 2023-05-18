5fb85ac41b20b71b658059396452c37d71dc18f5
Sebastian Mattias Rodriguez <seba.rodri.29@gmail.com>
2023-05-18 12:31:50 +0200

    First two requirement done. Tested greeting with name and greeting with null name

---
+++ Greeting.Test/UnitTest1.cs
@@ -0,0 +1,29 @@
+﻿namespace Greeting.Test;
+
+public class GreetingTest
+{
+    private readonly Greeting _sut;
+
+    public GreetingTest()
+    {
+        _sut = new Greeting();
+    }
+
+    [Fact]
+    public void SimpleGreet()
+    {
+        Assert.True(true);
+    }
+
+    [Fact]
+    public void ShouldReturnGreetPerson()
+    {
+        Assert.Equal("Hello, Bob", _sut.Greet("Bob"));
+    }
+
+    [Fact]
+    public void ShouldReturnGreetNullPerson()
+    {
+        Assert.Equal("Hello, my friend", _sut.Greet(null));
+    }
+}
---
+++ Greeting/Greeting.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Greeting
+{
+	public class Greeting
+	{
+		public Greeting()
+		{
+		}
+
+		public string Greet(string name)
+		{
+			if (name != null)
+			{
+               return $"Hello, {name}";
+            }
+			else
+			{
+				return "Hello, my friend";
+			}
+		}
+	}
+}
+
