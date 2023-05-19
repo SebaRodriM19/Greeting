3ca251de49b9034da5758fa1be924d47a12f76fe
Sebastian Mattias Rodriguez <seba.rodri.29@gmail.com>
2023-05-19 14:16:29 +0200

    Succesful execution tests

--- Greeting/Greeting.cs
+++ Greeting/Greeting.cs
@@ -14,7 +14,7 @@ namespace Greeting
 
             if (names.Length == 1)
             {
-                return names[0] == names[0].ToUpper() ? $"HELLO {names[0].ToUpper()}!" : $"Hello, {names}.";
+                return names[0] == names[0].ToUpper() ? $"HELLO {names[0].ToUpper()}!" : $"Hello, {names.First()}.";
             }
             else
             {
@@ -38,36 +38,35 @@ namespace Greeting
 
         private string GreetWithoutShoutingMorePerson(params string[] names)
         {
-            if (names.Length == 2)
-            {
-                return $"Hello, {names[0]} and {names[1]}.";
-            }
-            else if (AreMoreNamesInQuotes(names))
-            {
-               var namesNoQuotes = string.Join(",", names.Where(x => !x.Contains("\"")));
-               var namesQuotes = string.Join(",", names.Where(x => x.Contains("\"")).Select(x => string.Concat(x.Where(y => !y.Equals('\"')))));
-               var res = $"Hello, {namesNoQuotes} and {namesQuotes}.";
+                if (names.Length == 2)
+                {
+                    if (AreMoreNamesInQuotes(names))
+                    {
+                        var namesNoQuotes = string.Join(",", names.Where(x => !x.Contains("\"")));
+                        var namesQuotes = string.Join(",", names.Where(x => x.Contains("\"")).Select(x => string.Concat(x.Where(y => !y.Equals('\"')))));
+                        var res = $"Hello, {namesNoQuotes} and {namesQuotes}.";
 
-               return res;
-             }
-             else if (AreCommaInString(names))
-             {
-                var namesInAString = names.Where(x => x.Contains(",")).Select(x => x.Split(","));
-                var singleNames = string.Join(", ", names.Where(x => !x.Contains(",")));
-                var namesInASTringNoLast = string.Join(",", namesInAString.SkipLast(1));
-                var resMoreNameInAString = $"Hello, {string.Concat(singleNames, namesInASTringNoLast)}, {string.Join(", and", namesInAString.Last().Select(x => x))}.";
+                        return res;
+                    }
+                    else if (AreCommaInString(names))
+                    {
+                        var namesInAString = names.Where(x => x.Contains(",")).Select(x => x.Split(","));
+                        var singleNames = string.Join(", ", names.Where(x => !x.Contains(",")));
+                        var namesInASTringNoLast = string.Join(",", namesInAString.SkipLast(1));
+                        var resMoreNameInAString = $"Hello, {string.Concat(singleNames, namesInASTringNoLast)}, {string.Join(", and", namesInAString.Last().Select(x => x))}.";
 
-                return resMoreNameInAString;
+                        return resMoreNameInAString;
 
-             }
-             else
-             {
-                var nameAggregate = names.SkipLast(1)
-                                    .Aggregate((prev, next) => $"{prev}, {next}");
-                var res = $"Hello, {nameAggregate}, and {names.Last()}.";
-
-                return res;
-             }   
+                    }
+                    return $"Hello, {names[0]} and {names[1]}.";
+                }
+                else
+                {
+                    var nameAggregate = names.SkipLast(1)
+                                       .Aggregate((prev, next) => $"{prev}, {next}");
+                    var res = $"Hello, {nameAggregate}, and {names.Last()}.";
+                    return res;
+                }
         }
 
         private string GreetWithShouting(params string[] names)
